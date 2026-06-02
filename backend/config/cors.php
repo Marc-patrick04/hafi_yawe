ecc4b4f709e02c461412e5a94c22bd8da2f3b765
olivier20032 <usengimana2003@gmail.com>
2026-06-02 11:54:39 +0200

    init

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -4,7 +4,7 @@ return [
     'allowed_methods' => ['*'],
     'allowed_origins' => ['http://localhost:5173'],
     'allowed_origins_patterns' => [],
-    'allowed_headers' => ['*'],
+    'allowed_headers' => ['http://localhost:5173'],
     'exposed_headers' => [],
 'max_age' => 0,
 'supports_credentials' => true,
